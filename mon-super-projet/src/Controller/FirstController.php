9e323bd7b92b85baf3fdf0a803a2c92f9e7b9c15
Lucas <lucas.detling@gmail.com>
2023-10-03 17:59:12 +0200

    premier controller

---
+++ mon-super-projet/src/Controller/FirstController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FirstController extends AbstractController
+{
+    #[Route('/sayHello/{name}', name: 'say.hello')]
+    public function index(Request $request, $name): Response
+    {
+        dd($request);
+        return $this->render('first/index.html.twig', [
+            'controller_name' => 'FirstController',
+            'name' => $name,
+            'lastName' => 'Detling'
+        ]);
+    }
+}
